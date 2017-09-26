8c15f6464d77fffeb768cbf6a0fcb5650cd3d563
wocdomd <l.pinskiy@yahoo.com>
2017-09-26 13:57:33 -0400

    Refactored highlighting logic to decorate matched keywords instead of replacing them.

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,36 @@
 
-function scanAndHighlight(keywords) {
-  console.log('keywords:', keywords);
-  $('.essays2015-essay-content').each(function() {
-    let content = $(this).html();
-    const keywordArr = keywords.split(',');
-    for (let i = 0; i < keywordArr.length; i++) {
-      const regExp = new RegExp(keywordArr[i], "ig");
-      const substitute = "<span style='background:#FFFF00;'>" + keywordArr[i] + "</span>"; 
-      content = content.replace(regExp, substitute);
-    }
+function scanProfile(keywords) {
+  const keywordArr = keywords.split(',');
+  let regExpArr = [];
+  for (let i = 0; i < keywordArr.length; i++) {
+    // potential error here - need to match any part of word that matches keyword - not exact.
+    regExpArr.push(new RegExp(keywordArr[i], "ig"));
+  }
 
-    $(this).html(content);
+  $('.essays2015-essay-content').each(function() {
+    
+    let content = $(this).html().split(" ");
+    content = highlightContent(content, regExpArr);
+    $(this).html(content.join(" ")); // sketchy
+
   });
+}
+
+function highlightContent(textArr, regExpArr) {
+  if (!textArr || !regExpArr) {
+    console.error('Text passed into highlightContent is empty, or no keywords specified');
+    return;
+  }
+
+  for (let i = 0; i < textArr.length; i++) {
+    for (let j = 0; j < regExpArr.length; j++) {
+      if (textArr[i].match(regExpArr[j])) {
+        textArr[i] = "<span style='background:#FFFF00;'>" + text[i] + "</span>";
+        j = regExpArr.length; // break for loop. 
+      }
+    }
+  }
+  return textArr;
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -20,6 +39,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
 
   else if (request.type == 'scan') {
-    scanAndHighlight(request.keywords);
+    scanProfile(request.keywords);
   }
 });
